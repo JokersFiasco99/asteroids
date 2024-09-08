3676154d49b4d0bfdc799d944d9ac8e645e99830
JokersFiasco99 <sozinho.gil@gmail.com>
2024-09-08 11:42:45 +0200

    A: project setup and established infinite loop

---
+++ main.py
@@ -0,0 +1,32 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+
+    print("Starting asteroids!")
+
+    print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill((0, 0, 0))  # Fill the entire screen with black color
+        pygame.display.flip()
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
