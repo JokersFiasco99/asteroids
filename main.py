e891cf1112265835800be786c5345694b7af6aa1
JokersFiasco99 <sozinho.gil@gmail.com>
2024-09-08 12:06:58 +0200

    B: added player class and sprites

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
-
-    print("Starting asteroids!")
-
-    print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
 
     while True:
         for event in pygame.event.get():
@@ -16,7 +16,9 @@ def main():
                 return
 
         screen.fill((0, 0, 0))  # Fill the entire screen with black color
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 
